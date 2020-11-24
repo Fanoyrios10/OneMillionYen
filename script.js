a6ab985d9b4ea51d2ad355b0c37b5f6784db6580
fanoyrios <[aeroporoy10@hotmail.com]>
2020-11-24 16:19:35 +0200

    132

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+function open_answer_1() {
+  document.getElementById("close_eye_1").style.display = "none";
+  document.getElementById("show_option_1").style.backgroundColor = "yellow";
+  document.getElementById("show_option_1").innerHTML = "Batman";
+};
+
+function open_answer_2() {
+  document.getElementById("close_eye_2").style.display = "none";
+  document.getElementById("show_option_2").style.backgroundColor = "yellow";
+  document.getElementById("show_option_2").innerHTML = "Spiderman";
+};
+
+function open_answer_3() {
+  document.getElementById("close_eye_3").style.display = "none";
+  document.getElementById("show_option_3").style.backgroundColor = "yellow";
+  document.getElementById("show_option_3").innerHTML = "Wonder Woman";
+};
+
+function open_answer_4() {
+  document.getElementById("close_eye_4").style.display = "none";
+  document.getElementById("show_option_4").style.backgroundColor = "yellow";
+  document.getElementById("show_option_4").innerHTML = "Zoro";
+};
